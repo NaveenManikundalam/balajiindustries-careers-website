3f2a16d22d88c1cbcc19af07eaf73cf3468c64cd
Naveen Manikundalam <71772607+NaveenManikundalam@users.noreply.github.com>
2023-02-20 14:24:13 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-  return "Hello world!!. This is Naveen Kichcha."
+def hello_naveen():
+  return render_template('home.html')
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
-  
